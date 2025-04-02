67b05d8885adb751dbd3964c5775400890c80f70
soymaxhs <max_heredia95@hotmail.com>
2025-04-02 10:41:26 -0600

    refactor(Dashboard): HomePage updated to Dashboard to list users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
-import {Text} from "@mantine/core";
+"use client";
 
-export default function HomePage() {
+import { getUsers } from "@/services/users/api";
+import { Container, SimpleGrid, Title, Text, Code } from "@mantine/core";
+import { useEffect, useState } from "react";
+import { User } from "@/types";
+import UserCard from "@/components/molecules/UserCard";
+
+const COLORS = ["red", "green", "blue"];
+
+export default function Dashboard() {
+  const [users, setUsers] = useState<User[] | null>(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const usersData = await getUsers();
+      setUsers(usersData);
+    };
+
+    try {
+      fetchUsers();
+    } catch (error) {
+      console.log("Not users found", error);
+    }
+  }, []);
+
   return (
-    <Text>
-      HomePage
-    </Text>
+    <Container>
+      <Title mb="xl" c="dark">
+        Select Your User
+      </Title>
+      <SimpleGrid cols={3} spacing="lg">
+        {users?.map((user, index) => (
+          <UserCard key={user.id} user={user} bg={COLORS[index]} />
+        ))}
+      </SimpleGrid>
+      {/* TODO: Remove this. Only for reviewers */}
+      {!users && (
+        <>
+          <Text>
+            Did you forget to run the API server? Please run the following
+            command in a terminal:
+          </Text>
+          <Code>npm run api</Code>
+        </>
+      )}
+    </Container>
   );
 }
